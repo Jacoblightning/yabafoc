a49f4594c5f4fdaa9351dc81476953ee164fe8da
Jacob Freeman <t00niyf5@duck.com>
2025-07-13 14:29:32 -0400

    Multi-pass optimization

--- internal/optimize.py
+++ internal/optimize.py
@@ -19,69 +19,13 @@ def optimize_repeats(il: list[IL|int], single: IL, multi: IL) -> list[IL|int]:
         new_il.append(c)
     return new_il
             
-        
-
-def optimize_il(il: list[IL]) -> list[IL|int]:
+def optimize_zeros(il: list[IL|int]) -> list[IL|int]:
     new_il = []
 
-    addcount = 0
-    deccount = 0
-
-    rcount = 0
-    lcount = 0
     for c in range(len(il)):
-        # Repeated increments
-        if il[c] is IL.INC:
-            addcount += 1
-        else:
-            if addcount > 1:
-                new_il.append(IL.ADD)
-                new_il.append(addcount)
-                addcount = 0
-            elif addcount == 1:
-                new_il.append(IL.INC)
-                addcount = 0
-
-        # Repeated decrements
-        if il[c] is IL.DEC:
-            deccount += 1
-        else:
-            if deccount > 1:
-                new_il.append(IL.SUB)
-                new_il.append(deccount)
-                deccount = 0
-            elif deccount == 1:
-                new_il.append(IL.DEC)
-                deccount = 0
-
-        # Repeated rights
-        if il[c] is IL.NEXT:
-            rcount += 1
-        else:
-            if rcount > 1:
-                new_il.append(IL.SKIP)
-                new_il.append(rcount)
-                rcount = 0
-            elif rcount == 1:
-                new_il.append(IL.NEXT)
-                rcount = 0
-
-        # Repeated lefts
-        if il[c] is IL.PREV:
-            lcount += 1
-        else:
-            if lcount > 1:
-                new_il.append(IL.BACK)
-                new_il.append(lcount)
-                lcount = 0
-            elif lcount == 1:
-                new_il.append(IL.PREV)
-                lcount = 0
-
         # Optimizations past this point require a history of > 2
         if c < 2:
-            if addcount == deccount == rcount == lcount == 0:
-                new_il.append(il[c])
+            new_il.append(il[c])
             continue
 
         # Set 0
@@ -91,7 +35,17 @@ def optimize_il(il: list[IL]) -> list[IL|int]:
             new_il.append(0)
             continue
 
-        if addcount == deccount == rcount == lcount == 0:
-            new_il.append(il[c])
+        new_il.append(il[c])
+        
+    return new_il
 
+def optimize_il(il: list[IL]) -> list[IL|int]:
+
+    new_il = il
+
+    for s, m in ((IL.INC, IL.ADD), (IL.DEC, IL.SUB), (IL.NEXT, IL.SKIP), (IL.PREV, IL.BACK)):
+        new_il = optimize_repeats(new_il, s, m)
+
+    new_il = optimize_zeros(new_il)
+    
     return new_il
