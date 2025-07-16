5eba711ea3ada49bf2a93b1c9764a799e17e6351
Jacob Freeman <t00niyf5@duck.com>
2025-07-15 23:54:05 -0400

    Improve IL again

--- internal/optimize.py
+++ internal/optimize.py
@@ -13,10 +13,9 @@ def optimize_repeats(single: IL, multi: IL, il: list[IL|int]) -> list[IL|int]:
         if c is single:
             count += 1
             continue
-        else:
-            if count > 0:
-                new_il.append(multi)
-                new_il.append(count)
+        if count > 0:
+            new_il.append(multi)
+            new_il.append(count)
             count = 0
         new_il.append(c)
     return new_il
